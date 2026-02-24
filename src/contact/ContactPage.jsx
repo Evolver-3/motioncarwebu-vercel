3782b19ad1f0db309620c8f3c7d6c0bdc24e1f8e
Ashu Singh <Ashusingh2201@gmail.com>
2026-02-24 23:48:50 +0530

    contact page is now responsive

--- src/contact/ContactPage.jsx
+++ src/contact/ContactPage.jsx
@@ -33,10 +33,10 @@ const ContactPage = () => {
     }
   };
   return (
-    <div className='container my-30 px-10' id='Contact'>
-      <div className='grid grid-cols-1 sm:grid-cols-2 gap-10 md:gap-5'>
+    <div className='container my-10 px-3' id='Contact'>
+      <div className='flex gap-4 md:gap-5'>
 
-        <div className='space-y-8'>
+        <div className='space-y-8 w-1/2'>
           <motion.h1 initial={{opacity:0, y:-100 }}
           whileInView={{opacity:1,y:0}}
           transition={{
@@ -47,10 +47,10 @@ const ContactPage = () => {
           }}
           viewport={{once:true}}
            
-           className='text-4xl font-bold text-gray-600 font-mono'>Contact Us to buy our products from anywhere
+           className='text-2xl md:text-4xl font-bold text-gray-600 font-mono'>Contact Us to buy our products from anywhere
           </motion.h1>
 
-          <form onSubmit={onSubmit} className='gap-10 flex flex-col'>
+          <form onSubmit={onSubmit} className='gap-5  md:gap-10 flex flex-col w-full'>
           <motion.div 
           initial={{opacity:0, y:-100 }}
           whileInView={{opacity:1,y:0}}
@@ -59,9 +59,9 @@ const ContactPage = () => {
           }}
            className='flex items-center gap-5'>
 
-            <input className='w-[140px] lg:w-[150px] px-4 py-2 border border-gray-400 rounded-md' type="text" placeholder="Name"/>
+            <input className='w-1/2 md:w-2/3 px-4 py-2 border border-gray-400 rounded-md' type="text" placeholder="Name"/>
 
-            <input className='w-[200px] lg:w-[280px] px-4 py-2 border border-gray-400 rounded-md' type="email" placeholder='Email'/>
+            <input className='w-1/2 md:w-2/3 px-4 py-2 border border-gray-400 rounded-md' type="email" placeholder='Email'/>
 
           </motion.div>
 
@@ -73,25 +73,23 @@ const ContactPage = () => {
           }}
           className='flex items-center gap-5'>
 
-            <input className='w-[140px] lg:w-[150px] px-4 py-2 border border-gray-400 rounded-md' type="text" placeholder="Country"/>
+            <input className='w-1/2 md:w-2/3 px-4 py-2 border border-gray-400 rounded-md' type="text" placeholder="Country"/>
 
-            <input className='w-[200px] lg:w-[280px] px-4 py-2 border border-gray-400 rounded-md' type="text" placeholder='Zipcode'/>
+            <input className='w-1/2 md:w-2/3 px-4 py-2 border border-gray-400 rounded-md' type="text" placeholder='Zipcode'/>
 
           </motion.div>
-          <button className='px-2 py-2 text-lg font-bold bg-gray-600 text-white w-[365px] lg:w-[450px] rounded-lg hover:bg-gray-700 hover:text-orange-400'>{result?result:"Contact"}</button>
+          <button className=' w-full px-5 py-1.5 text-lg font-bold bg-gray-600 text-white rounded-lg hover:bg-gray-700 hover:text-orange-400'>{result?result:"Contact"}</button>
           </form>
         </div>
        
 
-        <div className='container w-120 relative'>
+        <div className=' w-1/2'>
 
-          <motion.div initial={{opacity:0,scale:0.5}} 
+          <motion.img initial={{opacity:0,scale:0.5}} 
           whileInView={{opacity:1,scale:1}}
           transition={{type:'spring',stiffness:100,damping:10,delay:1}}
-           className="absolute inset-0 w-[400px] md:w-[400px] lg:w-auto shadow-xl/60 rounded-md block ">
-            <img src={assets.upback} className='w-full h-full rounded-md'/>
-
-           </motion.div>
+           className=" w-full h-full shadow-xl/60 rounded-md block " src={assets.upback}>
+           </motion.img>
 
         </div>
       </div>
