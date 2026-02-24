39f060d8a8ba6df1bdef8e287ee6146c2e2ed1ad
Ashu Singh <Ashusingh2201@gmail.com>
2026-02-25 00:09:57 +0530

    Banner updated

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -2,61 +2,63 @@ import { assets } from "../assets"
 import {motion} from 'motion/react'
 
 const Banner = () => {
+
+  const containerVariant = {
+  hidden: {},
+  show: {
+    transition: {
+      staggerChildren: 0.2
+      }
+    }
+  }
+
+  const itemVariant = {
+  hidden: { opacity: 0, y: 80 },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: "spring",
+      stiffness: 100,
+      damping: 12
+      }
+    }
+  }
+
   return (
-    <div className='container my-20'>
-      <div className='background-image sm:min-h-[400px] sm:flex sm:justify-end sm:items-center rounded-lg text-white'>
+    <div className='w-full my-20 px-4'>
+      <div className="background-image px-10 w-full h-full sm:min-h-[400px] sm:flex sm:justify-end sm:items-center rounded-lg text-white">
+    
         <div className="">
           <motion.div
-          viewport={{once:true}}
+          variants={containerVariant}
+          initial="hidden"
+          animate='show'
           className="space-y-5 max-w-xl mx-auto ">
 
             <motion.h1 
-            initial={{opacity:0,y:100}}
-            whileInView={{opacity:1,y:0}}
-            transition={{
-              type:"spring",
-              stiffness:100,
-              damping:10,
-              delay:.3
-            }}
+            variants={itemVariant}
             className="text-6xl text-center md:text-8xl font-semibold leading-tight">Download the app
             </motion.h1>
 
             <motion.p 
-            initial={{opacity:0,y:100}}
-            whileInView={{opacity:1,y:0}}
-            transition={{
-              type:"spring",
-              stiffness:100,
-              damping:10,
-              delay:.4
-            }} className="text-center text-xl md:text-3xl sm:px-20">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa, ipsa?
+            variants={itemVariant}className="text-center text-xl md:text-3xl sm:px-20">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa, ipsa?
 
             </motion.p>
 
-            <div  className="flex justify-center gap-20 item-center">
+            <motion.div  className="flex justify-center gap-20 items-center"
+            variants={itemVariant}>
+
               <a href="#" className="max-w-[150px] sm:max-w-[120px] md:max-w-[200px]">
-                <motion.img
-                 initial={{opacity:0,y:100}}
-                 whileInView={{opacity:1,y:0}}
-                 transition={{
-                  type:"spring",
-                  stiffness:100,
-                  damping:10,
-                  delay:.4
-            }} src={assets.google}/>
+                <motion.img src={assets.google}/>
               </a>
+
               <a href="#" className="max-w-[150px] sm:max-w-[120px] md:max-w-[200px]" >
-                <motion.img initial={{opacity:0,y:100}}
-                whileInView={{opacity:1,y:0}}
-                transition={{
-                  type:"spring",
-                  stiffness:100,
-                  damping:10,
-                  delay:.5
-                  }} src={assets.apple}/>
+                <motion.img src={assets.apple}/>
               </a>
-            </div>
+            </motion.div>
+
+
           </motion.div>
         </div>
 
