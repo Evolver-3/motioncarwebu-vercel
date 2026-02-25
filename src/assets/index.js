6de25836a656b98b1332f2a8da6d2dbe538c1602
Ashu Singh <Ashusingh2201@gmail.com>
2026-02-25 11:04:09 +0530

    new car added

--- src/assets/index.js
+++ src/assets/index.js
@@ -15,8 +15,14 @@ import loca from './loca.svg'
 import tele from './tele.svg'
 import fb from './fb.svg'
 
+
+import tatapunch from './tata-punch.avif'
+import tatasierra from './tata-sierra.avif'
+import scorpio from './scorpio.avif'
+
+
 export const assets={
-  menu,car,facebook,insta,twitter,car1,car2,car3,upback,backu,google,apple,phone,loca,tele,fb
+  menu,car,facebook,insta,twitter,car1,car2,car3,upback,backu,google,apple,phone,loca,tele,fb,tatapunch,tatasierra,scorpio
 }
 
 export const secData=[
@@ -37,6 +43,24 @@ export const secData=[
   image:assets.car1,
   title:"Blue Car",
   subtitle:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Error deserunt consectetur"
+},
+{
+  id:4,
+  image:assets.tatapunch,
+  subtitle:"One of India’s best-selling cars, the Tata Punch, has received its first-ever major update. The 2026 Tata Punch facelift gets bolder exterior styling, a modernised cabin inspired by the Punch EV, and most importantly an all-new 1.2-litre turbo-petrol engine delivering 120 PS and 170 Nm. To add convenience, the carmaker has also provided it with a CNG AMT combination.",
+  price:"Rs. 5.60 - 10.55 Lakh*"
+},
+{
+  id:5,
+  image:assets.tatasierra,
+  subtitle:"The 2025 Tata Sierra has been launched at an introductory price of Rs 11.49 Lakh (ex-showroom). It will be offered in 4 broad Personas: Smart+, Pure, Adventure & Accomplished across 3 engine and 5 transmission options. Tata Sierra Pure variant price starts at Rs 12.99 lakh, whereas Tata Sierra Adventure variant price starts at Rs 15.29 lakh (ex-showroom). The Sierra will compete in the compact SUV space with cars like Hyundai Creta, Kia Seltos and Maruti Victoris.",
+  price:"Rs. 11.49 - 21.29 Lakh*"
+},
+{
+  id:5,
+  image:assets.scorpio,
+  subtitle:"Mahindra Scorpio N is amongst the most popular 7-seater SUVs in India, known for its tough build quality, reliable engines and off-road capabilities.The 2025 Scorpio N has a simple, old-school SUV design blended with modern elements such as projector headlights, LED daytime running lamps, 18-inch alloy wheels and vertically-stacked tail lamps. The Scorpio N Carbon is available for those who prefer an all-black look. ",
+  price:"Rs. 13.49 - 24.34 Lakh*"
 }
 ]
 
