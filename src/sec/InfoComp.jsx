fdac42012fba7c895f06152b788fc10a4e717013
Ashu Singh <Ashusingh2201@gmail.com>
2026-03-06 22:05:47 +0530

    final section page is now working properly working

--- src/sec/InfoComp.jsx
+++ src/sec/InfoComp.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const InfoComp = ({ car }) => {
+const InfoComp = ({ pickedCar }) => {
   return (
     <div className=" space-y-2 space-x-1 grid grid-cols-2 ">
-      {Object.entries(car.specs).map(([key, value]) => (
+      {Object.entries(pickedCar.specs).map(([key, value]) => (
         
           <div className='bg-white flex items-center justify-center  rounded-md w-40 h-10 mx-2 ' key={key}>
           <span className="font-semibold capitalize">{key}:</span>
